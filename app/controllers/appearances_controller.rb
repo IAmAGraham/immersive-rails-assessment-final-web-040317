593a5d63d5457ed83955b4c073832890c71fc925
github username <github email address>
2017-05-12 10:23:01 -0400

    Colbert Report Final

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,22 +1,48 @@
 class AppearancesController < ApplicationController
 
+  def index
+    @appearances = Appearance.all
+  end
+
+  def show
+    @appearance = Appearance.find(params[:id])
+  end
+
   def new
     @appearance = Appearance.new
   end
 
   def create
     @appearance = Appearance.new(appearance_params)
-    if @appearance.save
+    # @appearance.user_id = current_user.id
+    @appearance.save
       redirect_to @appearance.episode
+    # else
+      # redirect_to new_appearance_path
+    # end
+    # Wanted to make this work, but it's just not...
+  end
+
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    if @appearance.update(appearance_params)
+      redirect_to episode_path(@appearance.episode)
     else
-      redirect_to new_appearance_path
+      render 'edit'
     end
   end
+
+
+
 
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
   end
 
 end
